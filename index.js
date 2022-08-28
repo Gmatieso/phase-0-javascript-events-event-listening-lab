82bacc1265842f5dd65432e63dc7f826786ec33f
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-08-28 11:47:35 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
+
+// capturing the input 
+const input = document.getElementById('input');
+
+//adding event listener to the input variable
+input.addEventListener('click',() => alert("I was Clicked!"))
 }
+
+addingEventListener()
